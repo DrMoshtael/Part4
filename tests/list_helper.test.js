a115f254211aece45eb1b242035cb1048027917f
Howard <howard@brightpage.tech>
2024-01-04 18:02:33 +0000

    Exercise 4.5

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -7,39 +7,53 @@ test('dummy returns one', () => {
     expect(result).toBe(1)
 })
 
+const blog1 = {
+    "title":"Technology, Values, and the Shaping of Social Reality",
+    "author":"Matt Weinberg",
+    "url":"https://bahaiworld.bahai.org/library/technology-values-and-the-shaping-of-social-reality-2/",
+    "likes":1000,
+    "__v": 0
+}
+const blog2 = {
+    "title": "The Crisis of Identity",
+    "author": "Shahrzad Sabet",
+    "url":"https://bahaiworld.bahai.org/library/the-crisis-of-identity/",
+    "likes":950,
+    "__v": 1
+}
+
 describe('total likes', () => {
     test('of empty list is zero', () => {
         expect(listHelper.totalLikes([])).toBe(0)
     })
 
     test('when list has only 1 blog equals the like of that', () => {
-        const blog = {
-            "title":"Technology, Values, and the Shaping of Social Reality",
-            "author":"Matt Weinberg",
-            "url":"https://bahaiworld.bahai.org/library/technology-values-and-the-shaping-of-social-reality-2/",
-            "likes":1000,
-            "__v": 0
-        }
-        expect(listHelper.totalLikes([blog])).toBe(1000)
+        expect(listHelper.totalLikes([blog1])).toBe(1000)
     })
 
     test('of a bigger list is calculated right', () => {
-        const blogs = [
-            {
-                "title":"Technology, Values, and the Shaping of Social Reality",
-                "author":"Matt Weinberg",
-                "url":"https://bahaiworld.bahai.org/library/technology-values-and-the-shaping-of-social-reality-2/",
-                "likes":1000,
-                "__v": 0
-            },
-            {
-                "title": "The Crisis of Identity",
-                "author": "Shahrzad Sabet",
-                "url":"https://bahaiworld.bahai.org/library/the-crisis-of-identity/",
-                "likes":950,
-                "__v": 1
-            }
-        ]
-        expect(listHelper.totalLikes(blogs)).toBe(1950)
+        expect(listHelper.totalLikes([blog1,blog2])).toBe(1950)
+    })
+})
+
+describe('favourite blog', () => {
+    test('of an empty array is 0', () => {
+        expect(listHelper.favouriteBlog([])).toBe(0)
+    })
+
+    test('when a list has only 1 blog equals that blog', () => {
+        expect(listHelper.favouriteBlog([blog2])).toEqual({
+            "title": blog2.title,
+            "author": blog2.author,
+            "likes": blog2.likes
+        })
+    })
+
+    test('when a list has multiple blogs equals the blog with most likes', () => {
+        expect(listHelper.favouriteBlog([blog1, blog2])).toEqual({
+            "title": blog1.title,
+            "author": blog1.author,
+            "likes": blog1.likes
+        })
     })
 })
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -4,10 +4,29 @@ const dummy = (blogs) => {
     return 1
 }
 
-const totalLikes = (blogs) => blogs.reduce((accumulator, currentItem) => accumulator + currentItem.likes, 0)
+const totalLikes = (blogs) => {
+    return blogs.reduce((accumulator, currentItem) => accumulator + currentItem.likes, 0)
+}
 
+const favouriteBlog = (blogs) => {
+    const fav = blogs.reduce((favouriteBlog,currentBlog) => 
+    favouriteBlog = favouriteBlog.likes > currentBlog.likes
+        ? favouriteBlog
+        : currentBlog,
+    0
+    )
+
+    return fav === 0
+    ? 0
+    : {
+        "title": fav.title,
+        "author": fav.author,
+        "likes": fav.likes
+    }
+}
 
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favouriteBlog
 }
